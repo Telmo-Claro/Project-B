dcfa54ad67ff6b0a829ad658fc02a605fdce3427
Telmo-Claro <telmoclaro@proton.me>
2024-10-10 17:22:29 +0200

    up

--- ProgramTest/obj/Debug/net8.0/ProgramTest.AssemblyInfo.cs
+++ ProgramTest/obj/Debug/net8.0/ProgramTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProgramTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72352aeda88c50cd906f30b692252ecd3588d197")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProgramTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProgramTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
