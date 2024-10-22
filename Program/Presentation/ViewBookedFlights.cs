7f792fc51997908ca26c35ff050e2d2c5469269f
Axel <1059015@hr.nl>
2024-10-22 16:11:38 +0200

    Booked flights view

---
+++ Program/Presentation/ViewBookedFlights.cs
@@ -0,0 +1,22 @@
+public static class ViewBookedFlights
+{
+    public static void PrintBookedFlight(List<Flight> bookedFlights)
+    {
+        int i = 1;
+        Console.Clear();
+        foreach (Flight flight in bookedFlights)
+        {
+            Console.WriteLine(@$"Flight: {i}
+Flight number: {flight.FlightNumber}
+Departure: {flight.Departure}
+Destination: {flight.Destination}
+Date: {flight.Date}
+Aircraft name: {flight.Aircraft.Name}
+
+");
+            i++;
+        }
+        Console.WriteLine("Press any key to continue");
+        Console.ReadKey();
+    }
+}
