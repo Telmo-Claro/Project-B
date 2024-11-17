2a0cf72ea439e440bad913e444347116e3b030cb
Telmo-Claro <telmoclaro@proton.me>
2024-11-17 12:34:57 +0100

    up

--- Program/Presentation/WelcomingMenu.cs
+++ Program/Presentation/WelcomingMenu.cs
@@ -14,36 +14,25 @@
             Console.WriteLine("(2) Create an account");
             Console.WriteLine("(3) Exit");
             Console.Write("> ");
-            string userInput = Console.ReadKey().KeyChar.ToString();
-            MenuChoice(userInput);
-
-        }
-        // ReSharper disable once FunctionNeverReturns
-    }
-
-    public static void MenuChoice(string userInput)
-    {
-        switch (userInput)
-        {
-            case "1":
-                LoginMenu.Login();
-                break;
-            case "2":
-                CreateAccountMenu.CreateAccount();
-                break;
-            case "3":
-                Environment.Exit(0);
-                break;
-            case "p":
-                Admin.AdminMenu();
-                break;
-            //case "z":
-            //    OverviewAirbus330.Display330();
-            //    break;
-            default:
-                Console.WriteLine("\nInvalid option. Please try again.");
-                Console.ReadKey();
-                break;
+            switch (Console.ReadKey().KeyChar.ToString())
+            {
+                case "1":
+                    LoginMenu.Login();
+                    break;
+                case "2":
+                    CreateAccountMenu.CreateAccount();
+                    break;
+                case "3":
+                    Environment.Exit(0);
+                    break;
+                case "p":
+                    Admin.AdminMenu();
+                    break;
+                default:
+                    Console.WriteLine("\nInvalid option. Please try again.");
+                    Thread.Sleep(2000);
+                    break;
+            }
         }
     }
 }
