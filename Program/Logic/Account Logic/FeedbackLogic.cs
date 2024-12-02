260128e8de4fefb9c53af3efb33b1b4d55bcc6bc
Axel Warendorff <1059015@hr.nl>
2024-12-02 17:22:44 +0100

    Anonymous -_-

--- Program/Logic/Account Logic/FeedbackLogic.cs
+++ Program/Logic/Account Logic/FeedbackLogic.cs
@@ -1,9 +1,9 @@
 ﻿public static class FeedbackLogic
 {
-    public static void AddFeedback(Account account, string? message)
+    public static void AddFeedback(string? message)
     {
         var reviews = AppFeedbackRW.ReadJson();
-        Feedback newFeedback = new Feedback(account, message);
+        Feedback newFeedback = new Feedback(message);
         if (!reviews.Contains(newFeedback))
         {
             reviews.Add(newFeedback);
