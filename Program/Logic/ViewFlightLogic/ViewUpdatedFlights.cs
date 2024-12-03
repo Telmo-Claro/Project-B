807d9ceda224faf419d9e0ca59f433fbc642aa7f
Kacper <kacpersiudowski@gmail.com>
2024-12-03 16:03:01 +0100

    stoelen komen nu vrij als je booking canceled

--- Program/Logic/ViewFlightLogic/ViewUpdatedFlights.cs
+++ Program/Logic/ViewFlightLogic/ViewUpdatedFlights.cs
@@ -1,11 +1,11 @@
-﻿public static class ViewUpdatedFlights
-{
-    private static List<Flight> _flights = FlightDataRW.ReadJson();
+﻿using System.ComponentModel;
 
+public static class ViewUpdatedFlights
+{
     public static List<Flight> UpdateFlights()
     {
         var now = DateTime.Now;
-        
+        List<Flight> _flights = FlightDataRW.ReadJson();
         // Filter flights that have a date less than the current time.
         return _flights.Where(flight => flight != null && flight.Date > now).ToList();
     }
