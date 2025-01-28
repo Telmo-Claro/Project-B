2c24225e40291ac876b45ec9643c5d82e171b3f8
telmo <telmoclaro@proton.me>
2025-01-28 15:40:35 +0100

    IPay niet gebruikt fix v.3

--- Program/DataModels/IDeal.cs
+++ Program/DataModels/IDeal.cs
@@ -1,7 +1,7 @@
 ﻿using System.Diagnostics;
 public class IDeal : Payment
 {
-    public static new void Pay()
+    public static void Pay()
     {
         Process.Start(new ProcessStartInfo(_idealUrl) { UseShellExecute = true });
     }
